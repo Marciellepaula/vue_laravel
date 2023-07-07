8f857843047f6e2cc1c70de6a1a110614b82f93c
Marciellepaula <marcielle0644@gmail.com>
2023-07-07 16:36:14 -0300

    wip

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Minha Single Page Application</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    // Componente principal da SPA
+};
+</script>
+
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Página Inicial</h2>
+        <p>Bem-vindo à minha SPA!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // Componente da página inicial
+};
+</script>
